9aa2cee170bc64a5be5db39f5f3fdca1ede56771
BARHUG0 <bar23556@uvg.edu.gt>
2026-09-01 10:15:07 -0600

    chore: cleaned repo structure

--- lab3-routing/tests/test_dijkstra.py
+++
@@ -1,9 +0,0 @@
-"""
-DUEÑO: MJ (María José Girón)
-
-TODO: pruebas unitarias de node/algorithms/dijkstra/*
-- [ ] test_shortest_path_simple_graph
-- [ ] test_unreachable_node
-- [ ] test_topology_from_json
-- [ ] test_recompute_after_node_down
-"""
